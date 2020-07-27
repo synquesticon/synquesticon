5aece7e7180180b93acdabebb15a61dc15560b0a
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-07-27 20:03:54 +0200

    Update MQTTDialog.js

--- src/Header/Menu/Dialogs/MQTTDialog.js
+++ src/Header/Menu/Dialogs/MQTTDialog.js
@@ -1,111 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import Dialog from '@material-ui/core/Dialog';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+import Dialog from '@material-ui/core/Dialog'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-import * as mqtt from '../../../core/mqtt';
-import { set } from 'lodash';
+import * as mqtt from '../../../core/mqtt'
 
 const mqttDialog = (props) => {
+  const [useWSS, setUseWSS] = useState(true)
+  const [ipAddress, setIpAddress] = useState('127.0.0.1')
+  const [port, setPort] = useState('9001')
 
-  const [useWSS, setUseWSS] = useState(true);
-  const [ipAddress, setIpAddress] = useState('127.0.0.1');
-  const [port, setPort] = useState('9001');
-
-
-  useEffect(() => {
-    var mqttConfig = JSON.parse(props.myStorage.getItem('mqtt'));
-
-    const mqttObject = {
-      ip: ipAddress,
-      port: port,
-      bUseWSS: useWSS
-    };
-
-    if (mqttConfig){
-      mqtt.startMQTT(mqttConfig);
-      setIpAddress(mqttConfig.ip);
-      setPort(mqttConfig.port);
-      setUseWSS(mqttConfig.bUseWSS);
-    } else {
-      mqtt.startMQTT(mqttObject);
-    }
-
-  }, []);
-  
-
-
-  const onChangeMQTTSettings = (e) => {
-
-    const mqttObject = {
-      ip: ipAddress,
-      port: port,
-      bUseWSS: useWSS
-    };
-
-    props.myStorage.setItem('mqtt', JSON.stringify(mqttObject));
-
-    //Start MQTT and allow restart if there is an existing connection before
-    mqtt.startMQTT(mqttObject, true);
-
-    //Callback to close the dialog from the header
-    props.closeMQTTSettings();
+  const mqttObject = {
+    ip: ipAddress,
+    port: port,
+    bUseWSS: useWSS
   }
 
-  return(
+  useEffect(() => {
+    const mqttConfig = JSON.parse(props.myStorage.getItem('mqtt'))
+    if (mqttConfig) {
+      mqtt.startMQTT(mqttConfig)
+      setIpAddress(mqttConfig.ip)
+      setPort(mqttConfig.port)
+      setUseWSS(mqttConfig.bUseWSS)
+    } else {
+      mqtt.startMQTT(mqttObject)
+    }
+  }, [])
+
+  const onChangeMQTTSettings = (e) => {
+    props.myStorage.setItem('mqtt', JSON.stringify(mqttObject))
+
+    //Start MQTT and allow restart if there is an existing connection before
+    mqtt.startMQTT(mqttObject, true)
+
+    //Callback to close the dialog from the header
+    props.closeMQTTSettings()
+  }
+
+  return (
     <Dialog
-        open={props.openMQTTSettings}
-        onClose={props.closeMQTTSettings}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title" variant="h5">MQTT Settings</DialogTitle>
-        <DialogContent>
-          <TextField
-            required
-            padding="normal"
-            style={{marginRight:"10px", width:"calc(50% - 5px)"}}
-            id="mqttIP"
-            defaultValue={ipAddress}
-            label="MQTT IP Address"
-            onChange={(e) => setIpAddress(e.target.value)}
-          />
-          <TextField
-            required
-            padding="normal"
-            id="mqttPort"
-            defaultValue={port}
-            style={{width:"calc(50% - 5px)"}}
-            label="MQTT port"
-            type="number"
-            onChange={(e) => setPort(e.target.value)}
-          />
-          <FormControlLabel label="Use WSS"
-            value="end"
-            padding="dense"
-            id={"useWSS"}
-            checked={useWSS}
-            control={<Checkbox color="secondary" />}
-            onChange={(_, v) => setUseWSS(v)}
-            labelPlacement="end"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" onClick={props.closeMQTTSettings} >
-            Cancel
+      open={props.openMQTTSettings}
+      onClose={props.closeMQTTSettings}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title" variant="h5">MQTT Settings</DialogTitle>
+      <DialogContent>
+        <TextField
+          required
+          padding="normal"
+          style={{ marginRight: "10px", width: "calc(50% - 5px)" }}
+          id="mqttIP"
+          defaultValue={ipAddress}
+          label="MQTT IP Address"
+          onChange={(e) => setIpAddress(e.target.value)}
+        />
+        <TextField
+          required
+          padding="normal"
+          id="mqttPort"
+          defaultValue={port}
+          style={{ width: "calc(50% - 5px)" }}
+          label="MQTT port"
+          type="number"
+          onChange={(e) => setPort(e.target.value)}
+        />
+        <FormControlLabel label="Use WSS"
+          value="end"
+          padding="dense"
+          id={"useWSS"}
+          checked={useWSS}
+          control={<Checkbox color="secondary" />}
+          onChange={(_, v) => setUseWSS(v)}
+          labelPlacement="end"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" onClick={props.closeMQTTSettings} >
+          Cancel
           </Button>
-          <Button variant="outlined" onClick={onChangeMQTTSettings} >
-            OK
+        <Button variant="outlined" onClick={onChangeMQTTSettings} >
+          OK
           </Button>
-        </DialogActions>
+      </DialogActions>
     </Dialog>
-  );
+  )
 }
 
-export default mqttDialog;
+export default mqttDialog
