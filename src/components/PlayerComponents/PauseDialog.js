b39a3e7db2ef33ee785d07afd69a57a0db7c24fb
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-08-05 21:48:17 +0200

    Update PauseDialog.js

--- src/components/PlayerComponents/PauseDialog.js
+++ src/components/PlayerComponents/PauseDialog.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React from 'react'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Dialog from '@material-ui/core/Dialog'
+import { Typography } from '@material-ui/core'
 
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import { Typography } from '@material-ui/core';
-
-const PauseDialog = (props) => {
-  return(
-    <Dialog
-        open={props.openDialog}
-        onClose={props.closeDialog}
-        aria-labelledby="form-dialog-title"
-        fullWidth={true}
-        maxWidth='md'
-      >
-        <DialogTitle id="form-dialog-title" style={{ textAlign: 'center' }} disableTypography>
-          <Typography variant="h4" color="textPrimary">{props.pauseMessage}</Typography>
-        </DialogTitle>
-    </Dialog>
-  );
+const PauseDialog = props => {
+  return <Dialog
+    open={props.openDialog}
+    onClose={props.closeDialog}
+    aria-labelledby="form-dialog-title"
+    fullWidth={true}
+    maxWidth='md'>
+    <DialogTitle id="form-dialog-title" style={{ textAlign: 'center' }} disableTypography>
+      <Typography variant="h4" color="textPrimary">{props.pauseMessage}</Typography>
+    </DialogTitle>
+  </Dialog>
 }
 
-export default PauseDialog;
+export default PauseDialog
