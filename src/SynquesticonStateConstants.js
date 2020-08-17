6da9d5ac20d2b3f9e0b8599b62fcc371e0cd0385
potus84 <tuantai.cse@gmail.com>
2020-08-17 14:19:12 +0200

    Button

---
+++ src/SynquesticonStateConstants.js
@@ -0,0 +1,21 @@
+// session
+export const SESSION_START = 'SESSION_START'
+
+export const SESSION_END = 'SESSION_END'
+
+// set
+export const SET_START = 'SET_START'
+export const SET_END = 'SET_END'
+
+// task
+export const TASK_START = 'TASK_START'
+export const TASK_END = 'TASK_END'
+
+// componet
+// Button
+
+// Text
+
+// Number
+
+// Image
