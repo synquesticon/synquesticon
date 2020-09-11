7f6d6f3df2bb49cb3570467f2914b6007027291d
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-09-11 13:55:38 +0200

    Update beautifulDND.js

--- src/core/beautifulDND.js
+++ src/core/beautifulDND.js
@@ -1,41 +1,34 @@
 // a little function to help us with reordering the result
 export const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+}
 
-    return result;
-};
-
-/**
- * Moves an item from one list to another list.
- */
+//Moves an item from one list to another list.
 export const move = (source, destination, droppableSource, droppableDestination) => {
-    const sourceClone = Array.from(source);
-    const destClone = Array.from(destination);
+    const sourceClone = Array.from(source)
+    const destClone = Array.from(destination)
+    const draggedObject = sourceClone[droppableSource.index]
+    destClone.splice(droppableDestination.index, 0, draggedObject)
+    const result = {}
+    result[droppableSource.droppableId] = sourceClone
+    result[droppableDestination.droppableId] = destClone
+    return result
+}
 
-    //const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-    const draggedObject = sourceClone[droppableSource.index];
-
-    destClone.splice(droppableDestination.index, 0, draggedObject);
-
-    const result = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
-
-    return result;
-};
-
-export function getItemStyle(snapshot, draggableStyle, bgColor, highlightColor, noDropAnim){
-    return {...draggableStyle,
-            userSelect: 'none',
-            // change background colour if dragging
-            background: snapshot.isDragging ? highlightColor : bgColor,};
+export function getItemStyle(snapshot, draggableStyle, bgColor, highlightColor, noDropAnim) {
+    return {
+        ...draggableStyle,
+        userSelect: 'none',
+        // change background colour if dragging
+        background: snapshot.isDragging ? highlightColor : bgColor,
+    }
 }
 
 export const getListStyle = isDraggingOver => ({
     //background: isDraggingOver ? 'lightblue' : 'lightgrey',
     //padding: grid,
     //width: 250
-});
+})
