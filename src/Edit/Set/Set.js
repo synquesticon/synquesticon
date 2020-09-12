39bede7b5b3f6f538ac1548ec6149d0312981fd6
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-09-12 13:27:21 +0200

    Update Set.js

--- src/Edit/Set/Set.js
+++ src/Edit/Set/Set.js
@@ -18,11 +18,10 @@ class EditSet extends Component {
     //If we got a taskObject passed as a prop we use it, otherwise we init with a default constructed object
     //Clone the array via JSON. Otherwise we would operate directly on the original objects which we do not want
     this.set = this.props.isEditing ? JSON.parse(JSON.stringify(this.props.setObject)) : new dbObjects.TaskSetObject();
-    if (this.set.repeatSetThreshold === undefined) 
+    if (this.set.repeatSetThreshold === undefined)
       this.set.repeatSetThreshold = 0
-
-    //We keep these fields in the state as they affect how the component is rendered
-    this.state = {
+    
+    this.state = {      //We keep these fields in the state as they affect how the component is rendered
       taskList: this.set.childIds ? this.set.childIds : [],
       taskListObjects: [],
       randomizeSet: this.set.setTaskOrder === "Random" ? true : false,
@@ -37,12 +36,8 @@ class EditSet extends Component {
     this.handleUpdateSetChildTasks = this.onRetrievedSetChildTasksAddToSet.bind(this)
     this.handleSetTaskOrderChange = this.onSetTaskOrderChanged.bind(this)
     this.handleLogOneLineChange = this.onLogOneLineChanged.bind(this)
-
-    //The index where a new task will be placed
-    this.destinationIndex = 0
-
-    //Used to determine if the object should be closed
-    this.shouldCloseAsset = false
+    this.destinationIndex = 0           //The index where a new task will be placed
+    this.shouldCloseAsset = false       //Used to determine if the object should be closed
   }
 
   componentDidMount() {
@@ -54,40 +49,27 @@ class EditSet extends Component {
   }
 
   refreshSetChildList() {
-    if (this.state.taskList && this.state.taskList.length > 0) 
+    if (this.state.taskList && this.state.taskList.length > 0)
       db_helper.getTasksOrTaskSetsWithIDs(this.set._id, this.handleRetrieveSetChildTasks)
-    else  //If the list is empty we clear the list in the state
+    else      //If the list is empty we clear the list in the state
       this.setState({ taskListObjects: [] })
   }
 
   onRetrievedSetChildTasksAddToSet(retrievedObject) {
-    //Clone the array since we can't mutate the state directly
-    var updatedObjects = this.state.taskListObjects.slice()
-
-    //Replace the dummy object with the actual object
-    updatedObjects[this.destinationIndex] = retrievedObject
-
-    //Create a new task object for the taskList
-    var newTask = { id: retrievedObject._id, objType: retrievedObject.objType }
-
-    //Clone the array since we can't mutate the state directly
-    var updatedTaskList = this.state.taskList.slice()
-
-    //Replace the dummy object with the actual object
-    updatedTaskList[this.destinationIndex] = newTask
-
-    this.setState({
-      taskListObjects: updatedObjects,
-      taskList: updatedTaskList
-    })
+    let updatedObjects = this.state.taskListObjects.slice()
+    updatedObjects[this.destinationIndex] = retrievedObject           //Replace the dummy object with the actual object
+    let updatedTaskList = this.state.taskList.slice()
+    updatedTaskList[this.destinationIndex] = 
+      { id: retrievedObject._id, objType: retrievedObject.objType }   //Replace the dummy object with the actual object
+    this.setState({ taskListObjects: updatedObjects, taskList: updatedTaskList })
     this.set.childIds = updatedTaskList
   }
 
   updateSetChildList(taskToAdd) {
     this.shouldCloseAsset = false
-    if (taskToAdd.objType === dbObjects.ObjectTypes.SET) 
+    if (taskToAdd.objType === dbObjects.ObjectTypes.SET)
       db_helper.getTasksOrTaskSetsWithIDs(taskToAdd._id, this.handleUpdateSetChildTasks)
-    else if (taskToAdd.objType === dbObjects.ObjectTypes.TASK) 
+    else if (taskToAdd.objType === dbObjects.ObjectTypes.TASK)
       db_helper.getTaskWithID(taskToAdd._id, this.handleUpdateSetChildTasks)
   }
 
@@ -132,17 +114,16 @@ class EditSet extends Component {
     response = response.map((value) => { return value.trim() })
     response = response.filter(Boolean) //Remove empty values
 
-    if (target === "Tags") {
+    if (target === "Tags") 
       this.set.tags = response
-    } else if (target === "Repeat" && response[0]) {
+    else if (target === "Repeat" && response[0]) {
       response = response[0].replace(/\D/g, '')
       response = response === "" ? "0" : response
       this.set.repeatSetThreshold = Number(response)
     }
   }
 
-  //Callback from checkbox pressed
-  onSetTaskOrderChanged(e, checked) {
+  onSetTaskOrderChanged(e, checked) {                   //Callback from checkbox pressed
     this.set.setTaskOrder = checked ? "Random" : "InOrder"
     this.setState({ randomizeSet: checked })
   }
@@ -152,47 +133,34 @@ class EditSet extends Component {
     this.setState({ logOneLine: checked })
   }
 
-  //Returns true if adding the task will result in a circular reference
-  willCauseCircularReference(task) {
-    //We only need to check if the task we are adding is a TaskSet
-    if (task.objType === dbObjects.ObjectTypes.SET) {
-      //Try to get the data contained in the task set we are trying to add as we need this information to check for a circular reference
-      let queryList = []
+  willCauseCircularReference(task) {                    //Returns true if adding the task will result in a circular reference
+    if (task.objType === dbObjects.ObjectTypes.SET) {   //We only need to check if the task we are adding is a TaskSet
+      let queryList = []                                //Try to get the data contained in the task set we are trying to add as we need this information to check for a circular reference
       queryList.push({ id: task._id, objType: task.objType })
 
       db_helper.getTasksOrTaskSetsWithIDsPromise(task).then(data => {
-        //If the query was successful
-        if (data) {
-          //Extract the child set ids of the set we are trying to add as well as the set id
-          //Check that we are not adding a set containing the set we are editing now
-          if (this.getChildSetIDs(data, [data._id]).includes(this.set._id)) {
-            //If we are it would cause a circular reference
-            this.handleAddTaskAllowed(false, task, "The set you are trying to add already includes this set and would cause a circular reference");
+        if (data) {                                     //If the query was successful, extract the child set ids of the set we are trying to add as well as the set id
+          if (this.getChildSetIDs(data, [data._id]).includes(this.set._id)) {    //Check that we are not adding a set containing the set we are editing now. If we are it would cause a circular reference
+            this.handleAddTaskAllowed(false, task, "The set you are trying to add already includes this set and would cause a circular reference")
             return
           }
-          //No circular reference detected
-          const result_message = task.objType === dbObjects.ObjectTypes.SET ? "Set successfully added" : "Task successfully added";
+          const result_message = task.objType === dbObjects.ObjectTypes.SET ? "Set successfully added" : "Task successfully added"            //No circular reference detected
           this.handleAddTaskAllowed(true, task, result_message)
-        } else {    //Otherwise we do not add as we don't know if it will be ok
-          this.handleAddTaskAllowed(false, task, "Unable to query the database, did not add " + task.objType === dbObjects.ObjectTypes.SET ? "set" : "task");
-        }
+        } else this.handleAddTaskAllowed(false, task, "Unable to query the database, did not add " + task.objType === dbObjects.ObjectTypes.SET ? "set" : "task")  //Otherwise we do not add as we don't know if it will be ok
       })
     } else this.handleAddTaskAllowed(true, task, "Task successfully added")
   }
 
   getChildSetIDs(setObject, childSets) {
-    //Add the object to the list
-    childSets.push(setObject._id);
-
-    //Iterate over the sets children
-    setObject.data.forEach( obj => {
-      if (obj.objType === dbObjects.ObjectTypes.SET) this.getChildSetIDs(obj, childSets)
+    childSets.push(setObject._id)             //Add the object to the list
+    setObject.data.forEach(obj => {           //Iterate over the sets children
+      if (obj.objType === dbObjects.ObjectTypes.SET) 
+        this.getChildSetIDs(obj, childSets)
     })
     return childSets
   }
 
-  //Add a task to the list of tasks in the set
-  addTask(task, destinationIndex) {
+  addTask(task, destinationIndex) {           //Add a task to the list of tasks in the set
     if (this.set._id === task._id) {
       store.dispatch({
         type: 'TOAST_SNACKBAR_MESSAGE',
@@ -201,26 +169,19 @@ class EditSet extends Component {
       })
     } else {
       this.destinationIndex = destinationIndex
-      //perform a deeper check for circular references. This will in turn add the task if it is ok to do so.
-      this.willCauseCircularReference(task)
+      this.willCauseCircularReference(task)   //perform a deeper check for circular references. This will in turn add the task if it is ok to do so.
     }
   }
 
   handleAddTaskAllowed(allowed, task, message) {
     if (allowed) {
-      //Add a dummy object to the list while we wait for the callback
-      var dummyObject = { _id: task._id, question: "Adding...", objType: "Task", taskType: "Image" }
-      //Clone the array since we can't mutate the state directly
-      var updatedObjects = this.state.taskListObjects.slice()
-      //Insert the dummy at the index stored when add task was called
-      updatedObjects.splice(this.destinationIndex, 0, dummyObject)
+      let updatedObjects = this.state.taskListObjects.slice()   //Add a dummy object to the list while we wait for the callback
+      updatedObjects.splice(this.destinationIndex, 0,
+        { _id: task._id, question: "Adding...", objType: "Task", taskType: "Image" })
 
-      //Create a dummy task object for the taskList
-      var dummyTask = { id: task._id, objType: task.objType }
-      //Clone the array since we can't mutate the state directly
-      var updatedTaskList = this.state.taskList.slice()
-      //Insert the dummy task at the index stored when add task was called
-      updatedTaskList.splice(this.destinationIndex, 0, dummyTask)
+      let updatedTaskList = this.state.taskList.slice()         //Create a dummy task object for the taskList
+      updatedTaskList.splice(this.destinationIndex, 0,
+        { id: task._id, objType: task.objType })
 
       this.set.childIds = updatedTaskList
       this.updateSetChildList(task)
@@ -245,14 +206,14 @@ class EditSet extends Component {
   }
 
   removeTask(taskId) {   //Remove a task from the list of tasks in the set
-    var newList = [...this.state.taskList]
+    let newList = [...this.state.taskList]
     for (let i = 0; i < newList.length; i++) {
       if (newList[i].id === taskId) {
         newList.splice(i, 1)
         break
       }
     }
-    var newObjectList = [...this.state.taskListObjects]
+    let newObjectList = [...this.state.taskListObjects]
     for (let i = 0; i < newObjectList.length; i++) {
       if (newObjectList[i]._id === taskId) {
         newObjectList.splice(i, 1)
@@ -275,9 +236,9 @@ class EditSet extends Component {
   }
 
   moveTask(dragIndex, hoverIndex) {
-    var updatedTaskList = this.state.taskList.slice()
+    const updatedTaskList = this.state.taskList.slice()
     db_utils.arrayMove(updatedTaskList, dragIndex, hoverIndex)
-    var updatedObjectList = this.state.taskListObjects.slice()
+    const updatedObjectList = this.state.taskListObjects.slice()
     db_utils.arrayMove(updatedObjectList, dragIndex, hoverIndex)
 
     this.setState({
@@ -297,12 +258,11 @@ class EditSet extends Component {
       snackbarMessage: "Set deleted"
     })
 
-    db_helper.deleteTaskSetFromDb(this.set._id, this.handleDBCallback);
+    db_helper.deleteTaskSetFromDb(this.set._id, this.handleDBCallback)
   }
 
-  //Calls the provided callback function that handles the closing of this component
-  closeSetComponent(componentChanged, overrideShouldClose) {
-    let shouldClose = overrideShouldClose ? overrideShouldClose : this.shouldCloseAsset;
+  closeSetComponent(componentChanged, overrideShouldClose) {   //Calls the provided callback function that handles the closing of this component
+    let shouldClose = overrideShouldClose ? overrideShouldClose : this.shouldCloseAsset
     this.props.closeSetCallback(componentChanged, shouldClose)
   }
 
@@ -315,42 +275,34 @@ class EditSet extends Component {
 */
 
   render() {
-    var setContent =
-      <div>
-        <TextField id="questionText"
-          required
-          padding="dense"
-          defaultValue={this.set.name}
-          placeholder="Valve questions"
-          label="Set Name"
-          ref="setTextRef"
-          style={{ width: 'calc(50% - 10px)', marginRight: 10 }}
-          rows="1"
-          onChange={(e) => { this.set.name = e.target.value }}
-        />
-        <TextField id="tags"
-          required
-          padding="dense"
-          defaultValue={this.set.tags.join(',')}
-          placeholder="Pump, Steam"
-          label="Tags(comma-separated)"
-          style={{ width: '50%' }}
-          ref="tagsRef"
-          onChange={(e) => this.responseHandler(e, e.target.value, "Tags")}
-        />
-      </div>
-
-    let deleteTaskBtn = null
-    if (this.props.isEditing) {
-      deleteTaskBtn = <Button onClick={this.removeSet.bind(this)} variant="outlined">
-        Delete
-        </Button>
-    }
-
     return (
       <div className="setComponentContainer">
         <form className="setFormRoot" autoComplete="off" id="formRootId">
-          {setContent}
+          {
+            <div>
+              <TextField id="questionText"
+                required
+                padding="dense"
+                defaultValue={this.set.name}
+                placeholder="Valve questions"
+                label="Set Name"
+                ref="setTextRef"
+                style={{ width: 'calc(50% - 10px)', marginRight: 10 }}
+                rows="1"
+                onChange={(e) => { this.set.name = e.target.value }}
+              />
+              <TextField id="tags"
+                required
+                padding="dense"
+                defaultValue={this.set.tags.join(',')}
+                placeholder="Pump, Steam"
+                label="Tags(comma-separated)"
+                style={{ width: '50%' }}
+                ref="tagsRef"
+                onChange={(e) => this.responseHandler(e, e.target.value, "Tags")}
+              />
+            </div>
+          }
         </form>
 
         <div className="setTaskListContainer">
@@ -371,7 +323,12 @@ class EditSet extends Component {
           <Button onClick={this.onChangeSetSettings.bind(this)} variant="outlined">
             {this.props.isEditing ? "Save" : "Create"}
           </Button>
-          {deleteTaskBtn}
+          {this.props.isEditing ?
+            <Button onClick={this.removeSet.bind(this)} variant="outlined">
+              Delete
+            </Button>
+            : null
+          }
 
           <FormControlLabel label="Randomize order"
             value="start"
