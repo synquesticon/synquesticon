ae7b3157dc7b7d8625e193ec2bd33afa9e538f34
potus84 <tuantai.cse@gmail.com>
2021-01-08 15:04:18 +0100

    Model for answer component

---
+++ synquesticon_backend/models/answeredTaskComponentModel.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const extend = require('mongoose-schema-extend')
+const { ImageComponent, ButtonComponent, TextComponent, NumberComponent } =  require('./taskModel')
+const Schema = mongoose.Schema
+
+const HitAOIs = new Schema({
+   aois: [String], //names of the hit AOIS
+   x: Number,
+   y: Number,
+   timeClicked: Number,
+   _id: false
+})
+
+const isCorrectEnum = ['correct', 'incorrect', 'notApplicable', 'skip']
+
+const AnsweredTaskComponent = new Schema(
+    {
+
+        setId: {
+            type: Schema.Types.ObjectId,
+            required: true
+            
+        },
+        taskId: {
+            type: Schema.Types.ObjectId,
+            required: true
+            
+        },
+        componentId: {
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        /* correctlyAnswered:
+        1. If the participant answers correctly, we log it as “correct”.
+        2. If the participant answers incorrectly, we log it as “incorrect”.
+        3. If no correct answer was provided (i.e. the field “correct answer” in the editor is empty), we log it as “notApplicable”.
+        4. If the participant clicked “skip”, we log it as “skipped”, regardless of (3).
+        */
+        isCorrect: {
+            type: String,
+            enum: isCorrectEnum
+        },
+        
+        timeToFirtAnswer: Number,
+        firstResponseTimestamp: Number,
+        timeToCompletion: Number
+    },
+    { 
+        discriminatorKey: 'componentType',
+        collection: 'AnsweredComponent' 
+    }
+)
+
+const AnsweredTaskComponentBase = mongoose.model('AnsweredComponent', AnsweredTaskComponent)
+
+
+const AnsweredImageComponent = AnsweredTaskComponentBase.discriminator('Image', 
+    new Schema({
+        aoiCheckList: [HitAOIs]
+    })
+)
+
+
+const AnsweredButtonComponent = AnsweredTaskComponentBase.discriminator('Button', 
+    new Schema({
+        responseCountArray: [Number],
+        responseArray: [String]
+    })
+)
+
+const AnsweredTextComponent = AnsweredTaskComponentBase.discriminator('Text', 
+    new Schema({
+    response: String,
+    })
+)
+
+
+const AnsweredNumberComponent = AnsweredTaskComponentBase.discriminator('Number', 
+    new Schema({
+        response: Number
+    })
+)
+
+module.exports = AnsweredTaskComponent
